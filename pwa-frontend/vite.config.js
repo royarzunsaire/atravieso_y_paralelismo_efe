c20038d6116b1b9c9a1c239313bf998560b1c537
Rodrigo Alejandro Oyarzún Saire <r.oyarzunsaire@gmail.com>
2026-02-23 12:05:21 -0300

    update vite.config

--- pwa-frontend/vite.config.js
+++ pwa-frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 
 export default defineConfig({
